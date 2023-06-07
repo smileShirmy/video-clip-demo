4a60a0c6608f46d72712559d684e2f5e3a88e870
qiushiming <qiushiming1997@gmail.com>
2023-06-07 13:35:05 +0800

    feat(*): add video audio image

--- src/components/containers/player/FFmpegController.ts
+++ src/components/containers/player/FFmpegController.ts
@@ -1,5 +1,15 @@
-import { createFFmpeg } from '@ffmpeg/ffmpeg'
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg'
 import type { FFmpeg } from '@ffmpeg/ffmpeg'
+
+export const FileTypeMap = {
+  bpm: 'image/bpm',
+  png: 'image/png',
+  jpg: 'image/jpeg',
+  gif: 'image/gif',
+  mp3: 'audio/mp3',
+  mp4: 'video/mpeg4',
+  aac: 'audio/x-mei-aac'
+};
 
 interface FFmpegOptions {
   log?: boolean
@@ -15,6 +25,15 @@ export class FFmpegController {
   private ffmpeg: FFmpeg | Record<string, never> = {}
 
   private options: ReturnType<typeof normalizeFFmpegOptions>
+
+  paths = {
+    resource: '/resource/', //  资源目录，存放视频，音频等大文件
+    frame: '/frame/', // 持久化帧目录，用于轨道
+    palyFrame: '/play-frame/', // 播放帧文件
+    audio: '/audio/', // 合成音频文件
+    log: '/log/', // 命令日志文件目录
+    wave: '/wave/' // 音频波形文件目录
+  }
 
   constructor(options: FFmpegOptions = {}) {
     this.options = normalizeFFmpegOptions(options)
@@ -46,20 +65,49 @@ export class FFmpegController {
   }
 
   private initFileController() {
-    const paths = {
-      resource: '/resource/', //  资源目录，存放视频，音频等大文件
-      frame: '/frame/', // 持久化帧目录，用于轨道
-      palyFrame: '/play-frame/', // 播放帧文件
-      audio: '/audio/', // 合成音频文件
-      log: '/log/', // 命令日志文件目录
-      wave: '/wave/' // 音频波形文件目录
-    }
-
-    // 创建目录
-    Object.values(paths).forEach((path) => this.ffmpeg.FS('mkdir', path))
+    Object.values(this.paths).forEach((path) => this.ffmpeg.FS('mkdir', path))
   }
 
-  async splitAudioFromVideo() {
-    // 
+  async writeFile(fileDir: string, fileName: string, fileUrl: string) {
+    this.ffmpeg.FS('writeFile', `${fileDir}${fileName}`, await fetchFile(fileUrl));
+  }
+
+  async getFileUrl(filePath: string, fileName: string, format: string) {
+    const fileBlob = this.getFileBlob(filePath, fileName, format);
+    return window.URL.createObjectURL(fileBlob);
+  }
+
+  getFileBlob(filePath: string, fileName: string, format: string) {
+    const fileBuffer = this.getFileBuffer(filePath, fileName, format);
+    return new Blob([fileBuffer], { type: FileTypeMap[format as keyof typeof FileTypeMap] });
+}
+
+   // 获取文件buffer
+   getFileBuffer(filePath: string, fileName: string, format: string) {
+    const localPath = `${fileName}.${format}`;
+    return this.ffmpeg.FS('readFile', `${filePath}${localPath}`);
+}
+
+  async splitAudioFromVideo(videoName: string, format: string) {
+    // 转成 aac 格式
+    const audioName = `${videoName}.aac`;
+
+    // audio 路径
+    const audioPath = `${this.paths.resource}${audioName}`
+    const videoPath = `${this.paths.resource}${videoName}.${format}`
+    /**
+     * -i 设置输入文件名
+     * -v quiet 静默工作，不输出版本、工作信息
+     * -acodec 设置声音编码器
+     * copy 复制流
+     * -vn 不处理影像，仅针对声音做处理
+     */
+    const commands = ['-v', 'quiet', '-i', videoPath, '-acodec', 'copy', '-vn', audioPath]
+
+    await this.ffmpeg.run(...commands);
+    
+    return {
+      audioPath
+    }
   }
 }
--- src/components/containers/player/PlayerContainer.vue
+++ src/components/containers/player/PlayerContainer.vue
@@ -3,16 +3,24 @@ import { onMounted, ref } from 'vue';
 import { FFmpegController } from './FFmpegController';
 
 let loaded = ref(false);
-let ffmpegController: FFmpegController;
+let audioPath = ref('');
+let ffmpeg: FFmpegController;
 
 onMounted(async () => {
-  ffmpegController = new FFmpegController()
-  await ffmpegController.init();
+  ffmpeg = new FFmpegController()
+  await ffmpeg.init();
   loaded.value = true;
 })
 
-const splitAudioFromVideo = () => {
-  ffmpegController.splitAudioFromVideo()
+const splitAudioFromVideo = async () => {
+  const fileDir = ffmpeg.paths.resource;
+  const videoName = 'video-2'
+  const format = 'mp4'
+  const fileUrl = '/video/video-2.mp4'
+  // 获取 /public/ 下的 video/video-2.mp4 并且写入到内存中
+  await ffmpeg.writeFile(fileDir, `${videoName}.${format}`, fileUrl);
+  const res = await ffmpeg.splitAudioFromVideo(videoName, format)
+// ffmpeg.getFileUrl(ffmpeg.paths.audio, '')
 }
 </script>
 
@@ -20,6 +28,7 @@ const splitAudioFromVideo = () => {
   <aside>
     <button :disabled="!loaded" @click="splitAudioFromVideo()">从视频中分离音频</button>
   </aside>
+  <audio :src="audioPath" controls></audio>
 </template>
 
 <style scoped lang="scss">
